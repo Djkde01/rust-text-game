9db7d725a7c3d0e567bc72a9573b24349abe7580
Djkde01 <sergio.estrella@utp.edu.co>
2022-06-01 00:44:16 -0500

    feat(Implemented HistoryDataFormat intialization)

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,19 @@ struct HistoryDataFormat {
     health: i32,
 }
 
+impl HistoryDataFormat {
+    fn new(row: StringRecord) -> HistoryDataFormat {
+        let health = row.get(3).unwrap().parse::<i32>().unwrap_or(0);
+
+        HistoryDataFormat {
+            type_date: row.get(0).unwrap().trim().to_string(),
+            tag: row.get(1).unwrap().trim().to_string(),
+            text: row.get(2).unwrap().trim().to_string(),
+            health: health,
+        }
+    }
+}
+
 fn main() {
     let mut history_data: Vec<HistoryDataFormat> = Vec::new();
     let contents = fs::read_to_string(FILE_NAME).unwrap();
@@ -22,12 +35,7 @@ fn main() {
 
     for result in rdr.records() {
         let result = result.unwrap();
-        let data = HistoryDataFormat {
-            type_date: result.get(0).unwrap().trim().to_string(),
-            tag: result.get(1).unwrap().trim().to_string(),
-            text: result.get(2).unwrap().trim().to_string(),
-            health: 0,
-        };
+        let data = HistoryDataFormat::new(result);
         history_data.push(data);
     }
 
