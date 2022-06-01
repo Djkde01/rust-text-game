c43bf573dc9bce20adc65724215e31e97d80dc61
Djkde01 <sergio.estrella@utp.edu.co>
2022-06-01 00:33:12 -0500

    feat(Added HistoryDataFormat structure)

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,16 @@ use std::fs;
 
 const FILE_NAME: &str = "history.csv";
 
+#[derive(Debug)]
+struct HistoryDataFormat {
+    type_date: String,
+    tag: String,
+    text: String,
+    health: i32,
+}
+
 fn main() {
+    let mut history_data: Vec<HistoryDataFormat> = Vec::new();
     let contents = fs::read_to_string(FILE_NAME).unwrap();
 
     let mut rdr = ReaderBuilder::new()
@@ -12,6 +21,15 @@ fn main() {
         .from_reader(contents.as_bytes());
 
     for result in rdr.records() {
-        println!("Texto: {}", result.unwrap().get(2).unwrap());
+        let result = result.unwrap();
+        let data = HistoryDataFormat {
+            type_date: result.get(0).unwrap().trim().to_string(),
+            tag: result.get(1).unwrap().trim().to_string(),
+            text: result.get(2).unwrap().trim().to_string(),
+            health: 0,
+        };
+        history_data.push(data);
     }
+
+    println!("{:?}", history_data);
 }
