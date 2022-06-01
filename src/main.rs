2ca5b9dd1208129ebd5dcb5e570b107603f2b2dc
Djkde01 <sergio.estrella@utp.edu.co>
2022-06-01 00:16:57 -0500

    feat(Readed history in main file)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use csv::{ReaderBuilder, StringRecord};
+use std::fs;
+
+const FILE_NAME: &str = "history.csv";
+
 fn main() {
-    println!("Hello, world!");
+    let contents = fs::read_to_string(FILE_NAME).unwrap();
+
+    let mut rdr = ReaderBuilder::new()
+        .delimiter(b';')
+        .has_headers(false)
+        .from_reader(contents.as_bytes());
+
+    for result in rdr.records() {
+        println!("Texto: {}", result.unwrap().get(2).unwrap());
+    }
 }
