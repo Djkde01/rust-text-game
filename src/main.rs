7ca4809bea2b44afe23af002be05d011edc29e76
Djkde01 <sergio.estrella@utp.edu.co>
2022-06-01 23:50:46 -0500

    feat(Added situation tree)

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::collections::HashMap;
 use std::fs;
 
 const FILE_NAME: &str = "history.csv";
+const FIRST_TAG: &str = "INICIO";
 
 #[derive(Debug)]
 struct HistoryDataFormat {
@@ -10,6 +11,7 @@ struct HistoryDataFormat {
     tag: String,
     text: String,
     health: i32,
+    options: Vec<HistoryDataFormat>,
 }
 
 impl HistoryDataFormat {
@@ -21,11 +23,17 @@ impl HistoryDataFormat {
             tag: row.get(1).unwrap().trim().to_string(),
             text: row.get(2).unwrap().trim().to_string(),
             health: health,
+            options: Vec::new(),
         }
     }
 }
 
 fn main() {
+    let mut health = 100;
+    let mut current_tag = FIRST_TAG.to_string();
+
+    let mut last_record: String = String::new();
+
     let mut history_data: HashMap<String, HistoryDataFormat> = HashMap::new();
     let contents = fs::read_to_string(FILE_NAME).unwrap();
 
@@ -37,7 +45,17 @@ fn main() {
     for result in rdr.records() {
         let result = result.unwrap();
         let data = HistoryDataFormat::new(result);
-        history_data.insert(data.tag.clone(), data);
+
+        if data.type_date == "SITUACION" {
+            let record_tag = data.tag.clone();
+
+            history_data.insert(record_tag.clone(), data);
+            last_record = record_tag;
+        } else if data.type_date == "OPCION" {
+            if let Some(data_option) = history_data.get_mut(&last_record) {
+                (*data_option).options.push(data);
+            }
+        }
     }
 
     println!("{:?}", history_data);
